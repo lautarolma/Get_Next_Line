8dff716f5bb7272ffd423ecb5fcdfc93faece3a1
laviles <laviles@student.42madrid.com>
2025-11-17 17:36:07 +0100

    I've not idea whats going on here. I need a coffe or some sleep

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,46 @@
 /*   By: laviles <laviles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 06:23:12 by laviles           #+#    #+#             */
-/*   Updated: 2025/11/17 10:06:36 by laviles          ###   ########.fr       */
+/*   Updated: 2025/11/17 17:34:05 by laviles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	found_newline(char *buff, int len)
+char	*found_newline(char *buff, int len)
 {
+	int		i;
+
+	i = 0;
 	while (i++ < len)
-		if (buff[i++] != '\n')
-			
+	{
+		if (buff[i] == '\n')
+			return (&buff[i]);
+		else
+			i++;
+	}
+	return (NULL);
 }
 char	*line_reader(static char *current_line, int	fd)
 {
-	int		lines_read
+	int		chars_read
 	char	*buff;
+	char	*p;
 	int		i;
 
 	if (!current_line || fd < 0)
 		return (NULL);
-	buff = (char *)malloc(BUFFER_SIZE + 1);
+	buff = (char *)calloc(BUFFER_SIZE + 1);
 	if (!buff)
 		return (NULL);
-	while (lines_read != EOF)
+	while (chars_read != EOF)
 	{
-		lines_read = read(fd, buff, BUFFER_SIZE);
-		while (!found_newline(buff, lines_read))
-		{
-			buff[i]
-		}
-			
+		chars_read = read(fd, buff, BUFFER_SIZE);
+		p = found_newline(buff, chars_read);
+		if (!p)
+			memcpy(current_line, buff, chars_read);	
+		else
+			ft_strjoin(current_line, chars_read);
 	}
-	
+	return (current_line);
 }
 
 char	*get_next_line(int fd)
@@ -47,7 +56,7 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buff_reader, 0) < 0)
 		return (NULL);
-	current_line = NULL;
+	*current_line = NULL;
 	while (array[fd] != EOF)
 		current_line[i] = line_reader(array[fd])
 	
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: laviles <laviles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 03:31:49 by laviles           #+#    #+#             */
-/*   Updated: 2025/11/17 10:09:37 by laviles          ###   ########.fr       */
+/*   Updated: 2025/11/17 17:24:49 by laviles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void	*ft_calloc(size_t nb)
+void	*my_calloc(size_t nb)
 {
 	char	*ptr;
 	size_t	i;
@@ -29,4 +29,50 @@ void	*ft_calloc(size_t nb)
 		i++;
 	}
 	return (ptr);
+}
+
+char	*my_memcpy(char *dest, char *src, size_t n)
+{
+	size_t	i;
+	size_t	dest_len;
+	size_t	src_len;
+
+	if (!dest && !src)
+		return (NULL);
+	i = 0;
+	while (dest)
+		dest[i++];
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len = 0;
+	while (s1[len])
+		len++;
+	while (s2[len])
+		len++;
+	str = (char *)calloc((len), sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+		str[i] = s1[i++];
+	j = 0;
+	while (s2[j])
+		str[i + j] = s2[j++];
+	str[i + j] = '\0';
+	return (str);
 }
